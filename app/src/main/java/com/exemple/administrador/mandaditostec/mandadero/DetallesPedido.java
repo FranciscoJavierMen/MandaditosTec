476c0fa12aa734caa1cafdb396df4ed6acdb31a8
luis-baez <luis.baez.anaya@gmail.com>
2019-05-19 18:14:24 -0500

    Vista detalles pedido - mandadero

---
+++ app/src/main/java/com/exemple/administrador/mandaditostec/mandadero/DetallesPedido.java
@@ -0,0 +1,31 @@
+package com.exemple.administrador.mandaditostec.mandadero;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.administrador.mandaditostec.R;
+
+public class DetallesPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detalles_pedido);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
